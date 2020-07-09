344dc232d556848221bf5769eb5afdac1fddec49
agneswijayanti <agneswijayanti90@gmail.com>
2020-07-09 21:08:23 +0700

    llala

---
+++ resources/views/layouts/formkomentarjawaban.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts/master')
+
+@section('content')
+<h1>Form Komentar Pertanyaan</h1>
+<form action="/komentar/{{$jawaban[0]->idjawaban}}/jawaban" method="POST">
+{{ csrf_field() }}
+ 
+  <div class="form-group">
+    <label for="isi">ISI </label>
+    <textarea name="isi" id="isi" class="isi form-control" cols="30" rows="10"></textarea>
+  </div>
+  
+  
+ 
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+@endsection
+
+@push('scripts')
+<script src="https://cloud.tinymce.com/stable/tinymce.min.js"></script>
+<script>
+    tinymce.init({
+        selector:'textarea.isi',
+        width: 900,
+        height: 300
+    });
+</script>
+@endpush
+
+
+
--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,12 @@
 
 Route::get('/login', 'LoginController@home');
 Route::post('/login', 'LoginController@login');
+
 Route::get('/logout', 'LoginController@logout');
+
 Route::get('/register', 'LoginController@register');
 Route::post('/register', 'LoginController@registertambah');
+
 Route::get('/', 'PertanyaanController@home');
 Route::get('/pertanyaan/create', 'PertanyaanController@buat');
 Route::post('/pertanyaan', 'PertanyaanController@buatpertanyaan');
@@ -25,14 +28,15 @@ Route::put('/pertanyaan/{idpertanyaan}', 'PertanyaanController@editsimpan');
 Route::delete('/pertanyaan/{idpertanyaan}', 'PertanyaanController@delete');
 
 
-
 Route::get('/jawaban/create/{idpertanyaan}', 'JawabanController@form');
 Route::get('/jawaban/detail/{idjawaban}', 'JawabanController@jawabandetail');
 Route::post('/jawaban/{idpertanyaan}', 'JawabanController@buatsimpan');
 
 
 Route::get('/komentar/createpertanyaan/{idpertanyaan}', 'KomentarController@formpertanyaan');
+Route::get('/komentar/createjawaban/{idjawaban}', 'KomentarController@formjawaban');
 Route::post('/komentar/{idpertanyaan}/pertanyaan/', 'KomentarController@simpankomentarpertanyaan');
+Route::post('/komentar/{idjawaban}/jawaban/', 'KomentarController@simpankomentarjawaban');
 
 
 Route::get('/tandai/{idjawaban}/jawaban/{idpertanyaan}', 'VoteJawabanController@tandai');
@@ -42,3 +46,21 @@ Route::get('/up/{idjawaban}/jawaban/{idpertanyaan}', 'VoteJawabanController@up')
 
 Route::get('/up/{idpertanyaan}/pertanyaan', 'VotePertanyaanController@up');
 Route::get('/down/{idpertanyaan}/pertanyaan', 'VotePertanyaanController@down');
+
+
+// Route::get('/artikel', 'ArtikelController@artikel');
+// Route::get('/artikel/create', 'ArtikelController@create');
+// Route::post('/artikel', 'ArtikelController@simpan');
+// Route::get('/artikel/{id}', 'ArtikelController@show');
+// Route::get('/artikel/{id}/edit', 'ArtikelController@edit');
+// Route::put('/artikel/{id}', 'ArtikelController@editsimpan');
+// Route::delete('/artikel/{id}', 'ArtikelController@hapus');
+
+
+// Route::get('/items/create', 'ItemController@create'); // menampilkan halaman form
+// Route::post('/items', 'ItemController@store'); // menyimpan data
+// Route::get('/items', 'ItemController@index'); // menampilkan semua
+// Route::get('/items/{id}', 'ItemController@show'); // menampilkan detail item dengan id 
+// Route::get('/items/{id}/edit', 'ItemController@edit'); // menampilkan form untuk edit item
+// Route::put('/items/{id}', 'ItemController@update'); // menyimpan perubahan dari form edit
+// Route::delete('/items/{id}', 'ItemController@destroy'); // menghapus data dengan id
