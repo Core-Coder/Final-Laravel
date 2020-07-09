a55a1178ee523784b9f72bbffd92580263df315b
agneswijayanti <agneswijayanti90@gmail.com>
2020-07-09 19:44:50 +0700

    lalal

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -1,53 +1,27 @@
-<?php
+<?php 
 
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\login;
 use App\pertanyaan;
+use App\vote;
 use DB;
-
 class LoginController extends Controller
 {
-    public function login(Request $request)
-    {
-
+    public function login(Request $request){
+        
         $name = $request->input('username');
         $password = $request->input('password');
-        $user = DB::table('user')->where([['name', $name], ['pasword', $password]])->get();
-        if ($user->count() > 0) {
+        $user = DB::table('user')->where([['name', $name],['pasword',$password]])->get();
+        if($user->count()>0){
 
-            session()->put('name', $name);
-            session()->put('iduser', $user[0]->iduser);
+            session()->put('name',$name);
+            session()->put('iduser',$user[0]->iduser);
             $name = session()->get('name');
             $iduser = session()->get('iduser');
             // return $iduser;
             // die();
-
-            $pertanyaan = pertanyaan::all();
-            $user = [];
-
-            for ($i = 0; $i < count($pertanyaan); $i++) {
-                $user[] = login::where('iduser', $pertanyaan[$i]['iduser'])->get();
-            }
-
-
-            return view('layouts.home', ['pertanyaan' => $pertanyaan, 'user' => $user, 'iduser' => $iduser]);
-
-            die();
-        }
-
-        return view('layouts.login', ['data' => 'gagal login']);
-    }
-    public function home()
-    {
-        $name = session()->get('name');
-        if ($name != "") {
-            $name = session()->get('name');
-            $iduser = session()->get('iduser');
-            // return $iduser;
-            // die();
-<<<<<<< HEAD
            
                 $pertanyaan = pertanyaan::all();
                 $user=[];
@@ -59,6 +33,58 @@ class LoginController extends Controller
                
                 
                 return view('layouts.home', ['pertanyaan'=>$pertanyaan, 'user'=>$user, 'iduser'=>$iduser]);
+     
+            die();
+            
+        }
+            
+        return view('layouts.login',['data'=>'gagal login']);
+            
+        
+        
+        
+    }
+    public function home(){
+        $name = session()->get('name');
+        if($name!=""){
+            $name = session()->get('name');
+            $iduser = session()->get('iduser');
+            // return $iduser;
+            // die();
+            $pointuser=0;
+            if($iduser!=""){
+                $idpertanyaanpengguna = pertanyaan::where('iduser', $iduser)->get();
+            $idjawabanpengguna = pertanyaan::where('iduser', $iduser)->get();
+            $pointuser=0;
+            
+            for($i=0;$i<count($idpertanyaanpengguna);$i++){
+                $nilaivote = vote::where('idtujuan','pertanyaan,'.$idpertanyaanpengguna[$i]->idpertanyaan)->get();
+                
+                for($j=0;$j<count($nilaivote);$j++){
+                    $pointuser += $nilaivote[$j]->poin;
+                }
+            }
+            for($i=0;$i<count($idjawabanpengguna);$i++){
+                $nilaivote1 = vote::where('idtujuan','jawaban,'.$idjawabanpengguna[$i]->idjawaban)->get();
+                
+                for($j=0;$j<count($nilaivote1);$j++){
+                    $pointuser += $nilaivote1[$j]->poin;
+                }
+            }
+            
+
+            }
+        
+                $pertanyaan = pertanyaan::all();
+                $user=[];
+                
+                for($i=0;$i<count($pertanyaan);$i++){
+                    $user[] = login::where('iduser', $pertanyaan[$i]['iduser'])->get();
+                    
+                }
+               
+                
+                return view('layouts.home', ['pertanyaan'=>$pertanyaan, 'user'=>$user, 'iduser'=>$iduser,'poinuser'=>$pointuser]);
            
         }else{
         return view('layouts.login');
@@ -88,20 +114,3 @@ class LoginController extends Controller
         return redirect('/');
     }
 }
-=======
-
-            $pertanyaan = pertanyaan::all();
-            $user = [];
-
-            for ($i = 0; $i < count($pertanyaan); $i++) {
-                $user[] = login::where('iduser', $pertanyaan[$i]['iduser'])->get();
-            }
-
-
-            return view('layouts.home', ['pertanyaan' => $pertanyaan, 'user' => $user, 'iduser' => $iduser]);
-        } else {
-            return ('layouts.login');
-        }
-    }
-}
->>>>>>> e01a57a19eccc2ba16c3381797f73f85da737323
--- app/Http/Controllers/VotePertanyaanController.php
+++ app/Http/Controllers/VotePertanyaanController.php
@@ -9,56 +9,81 @@ use App\login;
 use App\komentar;
 use App\vote;
 use DB;
-
 class VotePertanyaanController extends Controller
 {
-    public function up($idpertanyaan)
-    {
+    public function up($idpertanyaan){
         $iduser = session()->get('iduser');
         $name = session()->get('name');
-        if ($name == "") {
-            return view('layouts.login', ['data' => 'belum login']);
-        } else {
-            $votetandai = vote::where([['idtujuan', 'pertanyaan,' . $idpertanyaan], ['poin', 10]])->get();
-            if ($votetandai->count() > 0) {
+        if($name=="")
+        {
+            return view('layouts.login',['data'=>'belum login']);   
+        }else{
+            $votetandai = vote::where([['idtujuan','pertanyaan,'.$idpertanyaan],['poin',10]])->get();
+           
+            if($votetandai->count()>0){
                 session()->put('uppertanyaan', 'tidak bisa upvote lagi');
-                return redirect('/pertanyaan/' . $idpertanyaan);
-            } else {
-
-                $data = array('poin' => 10, "idtujuan" => 'pertanyaan,' . $idpertanyaan, 'iduser' => $iduser);
+                return redirect('/pertanyaan/'.$idpertanyaan);
+            }else{
+                
+                $data=array('poin'=>10,"idtujuan"=>'pertanyaan,'.$idpertanyaan, 'iduser'=>$iduser);
                 DB::table('vote')->insert($data);
                 session()->put('uppertanyaan', 'berhasil upvote');
-                return redirect('/pertanyaan/' . $idpertanyaan);
+                return redirect('/pertanyaan/'.$idpertanyaan);
             }
         }
     }
-    public function down($idpertanyaan)
-    {
+    public function down($idpertanyaan){
         $iduser = session()->get('iduser');
         $name = session()->get('name');
-        if ($name == "") {
-            return view('layouts.login', ['data' => 'belum login']);
-        } else {
-            $votetandai = vote::where([['idtujuan', 'pertanyaan,' . $idpertanyaan], ['poin', -1]])->get();
-            if ($votetandai->count() > 0) {
-                session()->put('downpertanyaan', 'tidak bisa downvote lagi');
-                return redirect('/pertanyaan/' . $idpertanyaan);
-            } else {
-                $votetandai = vote::where([['idtujuan', 'pertanyaan,' . $idpertanyaan]])->get();
-                $jumlah = 0;
-                for ($i = 0; $i < count($votetandai); $i++) {
-                    $jumlah += $votetandai[$i]->poin;
-                }
-                if ($jumlah < 16) {
-                    session()->put('downpertanyaan', 'maaf tidak bisa downvote karena poin kurang dari 15');
-                    return redirect('/pertanyaan/' . $idpertanyaan);
-                } else {
-                    $data = array('poin' => -1, "idtujuan" => 'pertanyaan,' . $idpertanyaan, 'iduser' => $iduser);
-                    DB::table('vote')->insert($data);
-                    session()->put('downpertanyaan', 'berhasil downvote');
-                    return redirect('/pertanyaan/' . $idpertanyaan);
+        if($name=="")
+        {
+            return view('layouts.login',['data'=>'belum login']);   
+        }else{
+            $votetandai = vote::where([['idtujuan','pertanyaan,'.$idpertanyaan],['poin',-1]])->get();
+            $idpertanyaanpengguna = pertanyaan::where('iduser', $iduser)->get();
+            $idjawabanpengguna = pertanyaan::where('iduser', $iduser)->get();
+            $pointuser=0;
+            
+            for($i=0;$i<count($idpertanyaanpengguna);$i++){
+                $nilaivote = vote::where('idtujuan','pertanyaan,'.$idpertanyaanpengguna[$i]->idpertanyaan)->get();
+                
+                for($j=0;$j<count($nilaivote);$j++){
+                    $pointuser += $nilaivote[$j]->poin;
                 }
             }
-        }
+            for($i=0;$i<count($idjawabanpengguna);$i++){
+                $nilaivote1 = vote::where('idtujuan','jawaban,'.$idjawabanpengguna[$i]->idjawaban)->get();
+                
+                for($j=0;$j<count($nilaivote1);$j++){
+                    $pointuser += $nilaivote1[$j]->poin;
+                }
+            }
+            if($pointuser<16){
+                session()->put('downpertanyaan', 'poin anda minimal harus 15');
+                return redirect('/pertanyaan/'.$idpertanyaan);
+            }
+
+            else if($votetandai->count()>0){
+                session()->put('downpertanyaan', 'tidak bisa downvote lagi');
+                return redirect('/pertanyaan/'.$idpertanyaan);
+            }else{
+                $votetandai = vote::where([['idtujuan','pertanyaan,'.$idpertanyaan]])->get();
+                $jumlah = 0;
+                for($i=0;$i<count($votetandai);$i++){
+                    $jumlah+=$votetandai[$i]->poin;
+                }
+                if($jumlah<16){
+                    session()->put('downpertanyaan', 'maaf tidak bisa downvote karena poin kurang dari 15');
+                    return redirect('/pertanyaan/'.$idpertanyaan);
+                }else{
+                    $data=array('poin'=>-1,"idtujuan"=>'pertanyaan,'.$idpertanyaan, 'iduser'=>$iduser);
+                    DB::table('vote')->insert($data);
+                    session()->put('downpertanyaan', 'berhasil downvote');
+                    return redirect('/pertanyaan/'.$idpertanyaan);
+                }
+                
+            }
+    }
+    
     }
 }
